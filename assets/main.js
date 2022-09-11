833b6869914013e5c854c1d255f96e87397d8846
jirochanf <78668050+jirochanf@users.noreply.github.com>
2022-09-11 17:00:35 +0900

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -17,3 +17,19 @@ window.onload = (e)=>{
 		}
 	});
 }
+
+const click-play  => {
+	const video01 = document.getElementById("my_video01");
+	const btn01 = document.getElementById("my_btn01");
+
+	btn01.addEventListener("click", (e)=>{
+		if(video01.paused){
+			video01.play();
+			e.target.setAttribute("src", "#img_play");
+		}else{
+			video01.pause();
+			e.target.setAttribute("src", "#img_pause");
+		}
+	});
+}
+}
