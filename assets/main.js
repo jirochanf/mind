2432981b50dfe1e8dcbce2ddf8f4c6bc3a54086d
jirochanf <78668050+jirochanf@users.noreply.github.com>
2022-09-11 12:09:21 +0900

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+"use strict";
+
+// Window
+window.onload = (e)=>{
+
+	const video01 = document.getElementById("my_video01");
+	const btn01 = document.getElementById("my_btn01");
+
+	btn01.addEventListener("click", (e)=>{
+		if(video01.paused){
+			video01.play();
+			e.target.setAttribute("src", "#img_play");
+		}else{
+			video01.pause();
+			e.target.setAttribute("src", "#img_pause");
+		}
+	});
+}
