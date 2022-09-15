21484a46cb524feefdc4aefa0171c6efad14788c
jirochanf <78668050+jirochanf@users.noreply.github.com>
2022-09-16 06:04:29 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,78 +1,97 @@
-      const showInfo2 = () => {
+     const showInfo = () => {
         let y = 0;
-        const profileButton2 = document.querySelector("#profile-button");
-        const webButton2 = document.querySelector("#web-button");
-        const emailButton2 = document.querySelector("#email-button");
-        const locationButton2 = document.querySelector("#location-button");
-        const text2 = document.querySelector("#text");
-		const namecard2 = document.querySelector("#namecard");
+        const profileButton = document.querySelector("#profile-button");
+        const webButton = document.querySelector("#web-button");
+        const emailButton = document.querySelector("#email-button");
+        const locationButton = document.querySelector("#location-button");
+		const namecard = document.querySelector("#namecard");
 
-		  
-        profileButton2.setAttribute("visible", true);
+        profileButton.setAttribute("visible", true);
         setTimeout(() => {
-          webButton2.setAttribute("visible", true);
+          webButton.setAttribute("visible", true);
         }, 10);
         setTimeout(() => {
-          emailButton2.setAttribute("visible", true);
+          emailButton.setAttribute("visible", true);
         }, 10);
         setTimeout(() => {
-          locationButton2.setAttribute("visible", true);
+          locationButton.setAttribute("visible", true);
         }, 10);
 		setTimeout(() => {
-          namecard2.setAttribute("visible", true);
+          namecard.setAttribute("visible", true);
         }, 10);
 
+     }
 
-        //ポートフォリオ表示部　     
-        const showPortfolio2 = (done) => {
-        const portfolio2 = document.querySelector("#portfolio-panel2");
-        const portfolioLeftButton2 = document.querySelector("#portfolio-left-button2");
-        const portfolioRightButton2 = document.querySelector("#portfolio-right-button2");
-        const paintandquestPreviewButton2 = document.querySelector("#paintandquest-preview-button2");
-		const btn02 = document.getElementById("my_btn02");
+        const showPortfolio = (done) => {
+        const portfolio = document.querySelector("#panel");
+        const LeftButton = document.querySelector("#left-button");
+        const RightButton = document.querySelector("#right-button");
+        const PreviewButton = document.querySelector("#preview-button");
 	
         let y = 0;
         let currentItem = 0;
 
-        portfolio2.setAttribute("visible", true);
+        portfolio.setAttribute("visible", true);
 
-        const showPortfolioItem2 = (item) => {
+        const showPortfolioItem = (item) => {
           for (let i = 0; i <= 2; i++) {
-            document.querySelector("#portfolio2-item" + i).setAttribute("visible", i === item);
+            document.querySelector("#panel-item" + i).setAttribute("visible", i === item);
           }
         }
         const id = setInterval(() => {
           y += 0.008;
           if (y >= 0.6) {
             clearInterval(id);
-            portfolioLeftButton2.setAttribute("visible", true);
-            portfolioRightButton2.setAttribute("visible", true);
-            portfolioLeftButton2.addEventListener('click', () => {
+            LeftButton.setAttribute("visible", true);
+            RightButton.setAttribute("visible", true);
+            LeftButton.addEventListener('click', () => {
               currentItem = (currentItem + 1) % 3;
-              showPortfolioItem2(currentItem);
+              showPortfolioItem(currentItem);
             });
-            portfolioRightButton2.addEventListener('click', () => {
+            RightButton.addEventListener('click', () => {
               currentItem = (currentItem - 1 + 3) % 3;
-              showPortfolioItem2(currentItem);
+              showPortfolioItem(currentItem);
             });
 
-            paintandquestPreviewButton2.addEventListener('click', () => {
-              paintandquestPreviewButton2.setAttribute("visible", false);
+            PreviewButton.addEventListener('click', () => {
+              PreviewButton.setAttribute("visible", false);
               const testVideo = document.createElement( "video" );
               const canplayWebm = testVideo.canPlayType( 'video/webm; codecs="vp8, vorbis"' );
               if (canplayWebm == "") {
-                document.querySelector("#paintandquest-video-link2").setAttribute("src", "#paintandquest-video-mp42");
-                document.querySelector("#paintandquest-video-mp42").play();
+                document.querySelector("#video-link").setAttribute("src", "#video-mp4");
+                document.querySelector("#video-mp4").play();
               } else {
-                document.querySelector("#paintandquest-video-link2").setAttribute("src", "#paintandquest-video-webm2");
-                document.querySelector("#paintandquest-video-webm2").play();
+                document.querySelector("#video-link").setAttribute("src", "#video-webm");
+                document.querySelector("#video-webm").play();
               }
             });
-
 	
-            setTimeout2(() => {
+            setTimeout(() => {
               done();
             }, 500);
           }
-          portfolio2.setAttribute("position", "0 " + 0 + " -0.01");
-        }, 10);  
+          portfolio.setAttribute("position", "0 " + 0 + " -0.01");
+        }, 10);
+      }
+  
+      //  Aframeでターゲット0を摘出 
+AFRAME.registerComponent('mytarget', {
+        init: function () {
+          this.el.addEventListener('targetFound', event => {
+            console.log("target found");
+ //           showAvatar(() => {
+              setTimeout(() => {
+                showPortfolio(() => {
+                  setTimeout(() => {
+                    showInfo();
+                  }, 600);
+                });
+              }, 600);
+ //           });
+		  });
+          this.el.addEventListener('targetLost', event => {
+            console.log("target found");
+          });
+          //this.el.emit('targetFound');
+        }
+      });
