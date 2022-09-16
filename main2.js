a5c71835f1fc40e68b2e4f81536179651a72f97c
jirochanf <78668050+jirochanf@users.noreply.github.com>
2022-09-16 09:54:14 +0900

    Add files via upload

--- main2.js
+++ main2.js
@@ -1,10 +1,10 @@
    const showInfo = () => {
       let y = 0;
-    const profileButton = document.querySelector("#profile-button");
-      const webButton = document.querySelector("#web-button");
-      const emailButton = document.querySelector("#email-button");
-      const locationButton = document.querySelector("#location-button");
-      const namecard = document.querySelector("#namecard");
+    const profileButton = document.querySelector("#profile-button2");
+      const webButton = document.querySelector("#web-button2");
+      const emailButton = document.querySelector("#email-button2");
+      const locationButton = document.querySelector("#location-button2");
+      const namecard = document.querySelector("#namecard2");
 
       profileButton.setAttribute("visible", true);
       setTimeout(() => {
@@ -23,10 +23,10 @@
    }
 
       const showPortfolio = (done) => {
-      const portfolio = document.querySelector("#panel");
-      const LeftButton = document.querySelector("#left-button");
-      const RightButton = document.querySelector("#right-button");
-      const PreviewButton = document.querySelector("#preview-button");
+      const portfolio = document.querySelector("#panel2");
+      const LeftButton = document.querySelector("#left-button2");
+      const RightButton = document.querySelector("#right-button2");
+      const PreviewButton = document.querySelector("#preview-button2");
 
       let y = 0;
       let currentItem = 0;
@@ -35,7 +35,7 @@
 
       const showPortfolioItem = (item) => {
         for (let i = 0; i <= 2; i++) {
-          document.querySelector("#panel-item" + i).setAttribute("visible", i === item);
+          document.querySelector("#panel2-item" + i).setAttribute("visible", i === item);
         }
       }
       const id = setInterval(() => {
@@ -58,10 +58,10 @@
             const testVideo = document.createElement( "video" );
             const canplayWebm = testVideo.canPlayType( 'video/webm; codecs="vp8, vorbis"' );
             if (canplayWebm == "") {
-              document.querySelector("#video-link").setAttribute("src", "#video-mp4");
+              document.querySelector("#video-link2").setAttribute("src", "#video-mp4");
               document.querySelector("#video-mp4").play();
             } else {
-              document.querySelector("#video-link").setAttribute("src", "#video-webm");
+              document.querySelector("#video-link2").setAttribute("src", "#video-webm");
               document.querySelector("#video-webm").play();
             }
           });
